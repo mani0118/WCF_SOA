1e2490c19b56abed31128517f48c56cb7f987ea2
Abhishek Gopalan <abhi4717@gmail.com>
2013-03-16 14:54:14 +0530

    Run svcutil application to get the reference file and app.config

--- WCF_ClassLibrary/HelloWorld/Program.cs
+++ WCF_ClassLibrary/HelloWorld/Program.cs
@@ -1,10 +1,14 @@
 ﻿
+using System;
+
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var client = new HelloWorldServicesClient();
+            Console.WriteLine(client.GetMessage("Abhishek Gopalan"));
         }
     }
 }
