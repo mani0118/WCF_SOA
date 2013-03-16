45344cf83c210ca5ca88ee2ce0d77bad7fbcfe28
Abhishek Gopalan <abhi4717@gmail.com>
2013-03-16 19:41:37 +0530

    Hosted a WCF application on Console Application

---
+++ WCF_ClassLibrary/HostCmdLineApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel;
+using System.Configuration;
+using System;
+namespace HostCmdLineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceType = typeof(MyWCFServices.HelloWorldService);
+            var httpBaseAddress = ConfigurationSettings.AppSettings["HTTPBaseAddress"];
+            var baseAddress = new Uri[] { new Uri(httpBaseAddress) };
+            var host = new ServiceHost(serviceType, baseAddress);
+            host.Open();
+            Console.WriteLine("HelloWorldService is now running");
+            Console.WriteLine("Press any key to stop it....");
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+}
